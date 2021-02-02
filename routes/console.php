5a8f57c9bea4484b648602ee21c22ff3f94eba95
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-02-02 03:14:48 -0700

    Attempted to add an artisan command, but chose to create a permament alias for the path to phpunit for individual tests

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
