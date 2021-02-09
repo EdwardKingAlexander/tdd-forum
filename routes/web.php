6e774cf7f5e7a209bf55ebf1e585dd4364ac8eab
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-02-09 10:08:04 -0700

    changed the routes to reflect the new uri including the channel name

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+Route::get('/threads/{channel}/{thread}', [ThreadController::class, 'show']);
 Route::resource('threads', ThreadController::class);
-Route::post('/threads/{thread}/replies', [ReplyController::class, 'store']);
+Route::post('/threads/{channel}/{thread}/replies', [ReplyController::class, 'store']);
 require __DIR__.'/auth.php';
