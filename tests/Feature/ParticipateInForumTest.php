f5daa7c9744fbbe4446860811d92826a02e37aae
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-02-11 19:50:35 -0700

    added test to ensure that when a reply is posted to a thread, it requires a body

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -50,4 +50,22 @@ class ParticipateInForumTest extends TestCase
         $this->get($thread->path())
         ->assertSee($reply->body);
     }
+
+    /**
+     * @test
+     */
+    public function test_a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create(Thread::class);
+
+        $reply = make(Reply::class, ['body' => null]);
+
+        $this->post($thread->path(). '/replies', $reply->toArray())
+        ->assertSessionHasErrors('body');
+
+
+
+    }
 }
