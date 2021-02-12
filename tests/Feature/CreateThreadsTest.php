f02bb7b14cef4a65302ab2540848e3dc4b1099d7
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-02-11 19:33:44 -0700

    added tests to validate a required body, title, and valid channel id and added the validation in the controller to pass the tests

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Channel;
 use App\Models\Thread;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
@@ -50,6 +51,51 @@ class CreateThreadsTest extends TestCase
         ->assertSee($thread->title)
         ->assertSee($thread->body);
     }
+
+    /***
+     * @test
+     */
+     public function test_a_thread_requires_a_title()
+     {
+         $this->publishThread(['title' => null])
+         ->assertSessionHasErrors('title');
+     }
+
+      /***
+     * @test
+     */
+    public function test_a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+        ->assertSessionHasErrors('body');
+    }
+
+     /***
+     * @test
+     */
+    public function test_a_thread_requires_a_valid_channel()
+    {
+        Channel::factory()->count(2)->create();
+
+        $this->publishThread(['channel_id' => null])
+        ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 15])
+        ->assertSessionHasErrors('channel_id');
+    }
+
+        /**
+      * not
+      */
+      public function publishThread($overRides = [])
+      {
+         $this->withExceptionHandling()->signIn();
+ 
+         $thread = make(Thread::class, $overRides);
+ 
+         return $this->post('/threads', $thread->toArray());
+ 
+      }
 
 
 }
